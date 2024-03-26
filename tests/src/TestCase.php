8766e9461b5a52cd5971e5b0d5e474247b4050a4
lotyp <lotyp7@gmail.com>
2024-03-26 03:40:13 +0200

    feat: update structure and dependencies

--- tests/src/TestCase.php
+++ tests/src/TestCase.php
@@ -2,13 +2,34 @@
 
 declare(strict_types=1);
 
-namespace WayOfDev\Package\Tests;
+namespace WayOfDev\Tests;
 
+use Faker\Factory as FakerFactory;
+use Faker\Generator;
 use Orchestra\Testbench\TestCase as Orchestra;
 use WayOfDev\Package\Bridge\Laravel\Providers\PackageServiceProvider;
 
+use function array_key_exists;
+
 abstract class TestCase extends Orchestra
 {
+    final protected static function faker(string $locale = 'en_US'): Generator
+    {
+        /** @var array<string, Generator> $fakers */
+        static $fakers = [];
+
+        if (! array_key_exists($locale, $fakers)) {
+            $fakers[$locale] = FakerFactory::create($locale);
+        }
+
+        return $fakers[$locale];
+    }
+
+    public function ignorePackageDiscoveriesFrom(): array
+    {
+        return ['*', 'spatie/laravel-ray'];
+    }
+
     protected function getPackageProviders($app): array
     {
         return [
